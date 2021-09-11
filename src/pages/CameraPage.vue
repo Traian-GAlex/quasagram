db4181ce24f9a454bd7f851af3144b4b165b82c2
Traian-GAlex <traian.galex@gmx.com>
2021-09-11 22:33:38 +0200

    CameraPage Edit at Home

--- src/pages/CameraPage.vue
+++ src/pages/CameraPage.vue
@@ -1,6 +1,24 @@
 <template>
-  <q-page class="flex flex-center">
-    <h5>Camera page</h5>
+  <q-page class="constrain q-pa-md">
+    <div class="camera-frame q-pa-md">
+      <img class="full-width" src="https://cdn.quasar.dev/img/parallax2.jpg" alt="">
+    </div>
+    <div class="text-center q-pa-md">
+      <q-btn round
+             size="lg"
+             color="black"
+             icon="eva-camera"/>
+      <div class="row justify-center q-pa-md">
+        <q-input class="col" v-model="text" label="Caption" dense />
+      </div>
+      <div class="row justify-center q-pa-md">
+        <q-input class="col" v-model="text" label="Location" dense >
+          <template v-slot:append>
+            <q-btn round dense flat icon="eva-navigation-2-outline" />
+          </template>
+        </q-input>
+      </div>
+    </div>
   </q-page>
 </template>
 
@@ -9,3 +27,10 @@ export default {
   name: 'CameraPage'
 }
 </script>
+
+<style lang="scss">
+.camera-frame {
+  border: 2px solid $grey-10;
+  border-radius: 10px;
+}
+</style>
